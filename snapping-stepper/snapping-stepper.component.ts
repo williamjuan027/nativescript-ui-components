097b227aab96e1c9d452f611c1f0a58cc7d45793
williamjuan027 <williamjuan027@gmail.com>
2019-07-26 11:01:38 -0500

    Update snapping-stepper.component.ts

--- snapping-stepper/snapping-stepper.component.ts
+++ snapping-stepper/snapping-stepper.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnChanges, SimpleChanges, Input, Output, EventEmitter } from "@angular/core";
 import { GridLayout } from "ui/layouts/grid-layout";
 import { AnimationCurve } from "ui/enums";
+import { BehaviorSubject } from 'rxjs';
 
 import { StepperConfig } from "./stepperConfig";
+
+export const TOUCH_SPEED: number = 500; // set how fast you want the count to increment/decrement
 
 @Component({
 	selector: "app-snapping-stepper",
@@ -27,8 +30,20 @@ export class SnappingStepperComponent implements OnChanges {
 	@Output() valueChange = new EventEmitter<number>();
 
 	public stepCount: number = this.stepperConfig.startingNum;
+	private stepCountSubject: BehaviorSubject<number> = new BehaviorSubject(this.stepCount);
 
 	private prevDeltaX: number = 0;
+
+	private touchOpt: { timer: any, interval: number } = {
+		timer: null,
+		interval: TOUCH_SPEED
+	};
+
+	private panOpt: { timer: any, timerCount: number, direction: 'left' | 'right' | null } = {
+		timer: null,
+		timerCount: 0,
+		direction: null
+	};
 
 	constructor() {
 	}
@@ -37,20 +52,74 @@ export class SnappingStepperComponent implements OnChanges {
 
 	}
 
-	stepNegative() {
-		// decrement if still within bounds
-		if (this.stepCount > this.stepperConfig.limitLower) {
-			this.stepCount -= 1;
-			this.valueChange.emit(this.stepCount);
+	// private touchTimer = null;
+	onStepTouch(args, state: 'positive' | 'negative') {
+		// touch only gets triggered on first down, then when the fingers move, and up
+		// it doesn't keep firing when you hold down your finger
+		if (args.action === 'down') {
+			// clear timer before starting new timer (in case there is a timer already running);
+			this.clearTouchTimer();
+			// start the timer when finger is first down
+			this.startTouchTimer(state);
+		} else if (args.action === 'up') {
+			// kill timer after finger is lifted
+			this.clearTouchTimer();
+			this.emitCountValue();
 		}
 	}
 
-	stepPositive() {
+	startTouchTimer(state: 'positive' | 'negative'): void {
+		if (state === 'positive' && this.stepCount < this.stepperConfig.limitUpper) {
+			this.stepPositive(false);
+			this.touchOpt.interval = this.touchOpt.interval * 0.8;	// accelerate
+			this.touchOpt.timer = setTimeout(this.startTouchTimer.bind(this, state), this.touchOpt.interval);
+		} else if (state === 'negative' && this.stepCount > this.stepperConfig.limitLower) {
+			this.stepNegative(false);
+			this.touchOpt.interval = this.touchOpt.interval * 0.8;  // accelerate
+			this.touchOpt.timer = setTimeout(this.startTouchTimer.bind(this, state), this.touchOpt.interval);
+		} else {
+			// done counting
+			this.clearTouchTimer();
+		}
+	}
+
+	clearTouchTimer(): void {
+		// if timer exist, stop and set it to null
+		if (this.touchOpt.timer) {
+			clearTimeout(this.touchOpt.timer);
+			this.touchOpt.timer = null;
+		}
+		// reset interval to initial speed
+		this.touchOpt.interval = TOUCH_SPEED;
+	}
+
+	stepNegative(shouldEmitValue: boolean = true): void {
+		// decrement if still within bounds
+		if (this.stepCount > this.stepperConfig.limitLower) {
+			this.stepCount -= 1;
+			this.stepCountSubject.next(this.stepCount);
+
+			// TODO: emit only when its settled
+			if (shouldEmitValue) {
+				this.emitCountValue();
+			}
+		}
+	}
+
+	stepPositive(shouldEmitValue: boolean = true): void {
 		// increment if still within bounds
 		if (this.stepCount < this.stepperConfig.limitUpper) {
 			this.stepCount += 1;
-			this.valueChange.emit(this.stepCount);
+			this.stepCountSubject.next(this.stepCount);
+
+			if (shouldEmitValue) {
+				this.emitCountValue();
+			}
 		}
+	}
+
+	emitCountValue(): void {
+		this.valueChange.emit(this.stepCount);
 	}
 
 	onCountPan(args) {
@@ -64,17 +133,28 @@ export class SnappingStepperComponent implements OnChanges {
 			// finger moving
 			if (Math.abs(newX) < this.stepperConfig.width / 3) {
 				grdLayout.translateX = newX;
+
+				// increment or decrement stepper depending on pan direction
+				// only increment if panning is still within bounds
+				if (newX > 0 && this.panOpt.direction !== 'right') {
+					// pan right
+					this.clearPanTimer();
+					this.panOpt.timer = setInterval(() => {
+						this.stepPositive();
+					}, 10);
+				} else if (newX <= 0 && this.panOpt.direction !== 'left') {
+					// pan left
+					this.clearPanTimer();
+					this.panOpt.timer = setInterval(() => {
+						this.stepNegative();
+					}, 10);
+				}
+			} else {
+				// out of bounds
+				console.log('out of bounds?');
+				// this.clearPanTimer();
 			}
 			this.prevDeltaX = args.deltaX;
-
-			// increment or decrement stepper depending on pan direction
-			if (newX > 0) {
-				// pan right
-				this.stepPositive();
-			} else {
-				// pan left
-				this.stepNegative();
-			}
 		} else if (args.state === 3) {
 			// finger up
 			this.prevDeltaX = 0;
@@ -85,6 +165,15 @@ export class SnappingStepperComponent implements OnChanges {
 				curve: AnimationCurve.cubicBezier(0, 0.405, 0, 1.285),
 				duration: 200
 			});
+			this.panOpt.direction = null;
+			this.clearPanTimer();
+		}
+	}
+
+	clearPanTimer() {
+		if (this.panOpt.timer) {
+			clearInterval(this.panOpt.timer);
+			this.panOpt.timer = null;
 		}
 	}
 }
